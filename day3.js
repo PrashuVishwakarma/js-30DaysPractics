4a970080399d3ef8056b49defe9aabbabad34cdd
Prashu Vishwakarma <prashuvish100@gmail.com>
2025-06-03 15:57:04 +0530

    Operators in js

---
+++ day3.js
@@ -0,0 +1,23 @@
+/*
+Operators in javascript
+Operators is sysmbol which provide to the operands to perform specific task or operation
+
+There are different types of JavaScript operators:
+
+Arithmetic Operators
+Assignment Operators
+Comparison Operators
+String Operators
+Logical Operators
+Bitwise Operators
+Ternary Operators
+Type Operators
+*/
+
+// Assign the value 18 to a
+let a = 18;
+// Assign the value 5 to b
+let b = 5
+// Assign the value a + b to c
+let c = a + b;
+console.log(c);
