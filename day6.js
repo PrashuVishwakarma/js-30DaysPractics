f9e8ab85d44c61498b95f02e55a5100d1c170ebe
Prashu Vishwakarma <prashuvish100@gmail.com>
2025-06-07 20:27:15 +0530

    Function

---
+++ day6.js
@@ -0,0 +1,10 @@
+/* function : function is a set of block which is used
+to perform some specific task.
+*/
+```
+function functionName(parameter1, parameter2, ...) 
+{
+    //function body here
+    return value; // Optional
+}
+```
