cd93704c6bf16c780bf57c5f0b8d9f51eae71624
Prashu Vishwakarma <prashuvish100@gmail.com>
2025-06-09 00:24:33 +0530

    Function in JavaScript

--- day6.js
+++ day6.js
@@ -1,10 +1,38 @@
 /* function : function is a set of block which is used
 to perform some specific task.
 */
-```
-function functionName(parameter1, parameter2, ...) 
-{
-    //function body here
-    return value; // Optional
+
+// function functionName(parameter1, parameter2, ...) 
+// {
+//     //function body here
+//     return value; // Optional
+// }
+
+function square(number){
+    return number * number
 }
-```
+console.log(square(9))
+
+function newValue(x,y){
+    return x + y
+}
+console.log(newValue(55,45))
+
+function helloMsg(name){
+    return ("hello "+name+" how are you")
+}
+console.log(helloMsg("Guys"))
+console.log(helloMsg("Guys"))
+console.log(helloMsg("Guys"))
+
+//Function Expression
+let addNum = function num(x,y){
+    return x + y
+
+}
+console.log(addNum(2,2))
+
+//Arrow Functions
+const add = (a, b) => a + b;
+
+console.log(add(5, 3));
