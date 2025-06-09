8aad600b4779f0056a3686b3f2fb0f43252c04be
Prashu Vishwakarma <prashuvish100@gmail.com>
2025-06-10 00:01:17 +0530

    Array

---
+++ day7.js
@@ -0,0 +1,17 @@
+//Arrays : Arrays is used to store the multiple value in single variable
+
+let fruits = ["Apple","Banana","Orange","Mango"]
+console.log(fruits[2]);
+console.log(fruits[0]);
+
+// Common Array method 
+
+/**push()      add value at end
+ * pop()       remove last value
+ * shift()     remove first value
+ * unshift()   add value at beginning
+ * length()    get array size
+ * indexOf()   find index of value
+ * map()       transform each value
+ * filter()    filter the elements
+*/
