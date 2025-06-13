b4bc7e8707753f37304e41bb1e3aff6922b80415
Prashu Vishwakarma <prashuvish100@gmail.com>
2025-06-13 19:28:56 +0530

    Array in JavaScript or Methods

--- day7.js
+++ day7.js
@@ -13,5 +13,28 @@ console.log(fruits[0]);
  * length()    get array size
  * indexOf()   find index of value
  * map()       transform each value
- * filter()    filter the elements
+ * slice()     it output return the given range and remove the out out of range, they not affect original array
+ * splice()    removing or replacing existing elements 
 */
+
+let carName = ["BMW", "Audio", "Range Rover"];
+carName.push("Urus","Porche");   //'BMW', 'Audio', 'Range Rover', 'Urus', 'Porche'
+console.log(carName);
+
+let bikeName = ["NinjaH2R", "Hunter", "R15","PulserNs"];
+bikeName.pop();               //NinjaH2R", "Hunter", "R15"
+bikeName.shift();             //"Hunter", "R15"
+bikeName.unshift("ZMR");      //"ZMR", "Hunter", "R15"
+console.log(bikeName[0]);     //"ZMR"
+console.log(bikeName.length); //3
+  
+
+const animals = ["ant", "bison", "camel", "duck", "elephant"];
+console.log(animals.slice(2));      // "camel", "duck", "elephant"
+console.log(animals.slice(2,4));   //   "camel", "duck"
+
+const months = ["Jan", "March", "April", "June"];
+months.splice(1,0, "feb");
+console.log(months);   //"Jan","feb", "March", "April", "June"
+months.splice(4,1,"May");
+console.log(months);  //'Jan', 'feb', 'March', 'April', 'May' 
