49c7adb1be3b90e0a82c2f4cd2d5d63637d79097
Prashu Vishwakarma <prashuvish100@gmail.com>
2025-06-16 19:43:43 +0530

    Array Methods

---
+++ day10.js
@@ -0,0 +1,20 @@
+/*Most Commonly Used Methods
+-----------------------------------------
+push() - Adding elements
+pop() - Removing elements
+map() - Transforming data
+filter() - Filtering data
+forEach() - Iterating through array
+find() - Finding specific elements
+includes() - Checking existence
+slice() - Creating subarrays
+concat() - Combining arrays
+reduce() - Aggregating data */
+
+const num = [1,2,3,4,5];
+console.log(num.push(6));  //6
+
+let fruits = ['apple', 'banana', 'orange']; 
+let removed = fruits.pop(); 
+console.log(fruits); // ['apple', 'banana'] 
+console.log(removed); // 'orange'
