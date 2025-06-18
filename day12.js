0840e3331c864d23c45060b7d007b39b1914f7ef
Prashu Vishwakarma <prashuvish100@gmail.com>
2025-06-18 15:12:25 +0530

    Add Method Math object

--- day12.js
+++ day12.js
@@ -1,5 +1,23 @@
 //Math object : math object in built in object to provide properites and method for mathematical function
 
-console.log(Math.PI);
-console.log(Math.LN2);
-console.log(Math.E);
+console.log(Math.PI); //3.141592653589793
+
+console.log(Math.LN2); //0.6931471805599453
+
+console.log(Math.E);   //2.718281828459045
+
+console.log(Math.round(2.3));      // 2
+console.log(Math.max(1,5,3,9,5));  //9
+console.log(Math.min(8,2,4,1,9));  //1
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10));
+
+console.log(Math.pow(2,3));  //8
+console.log(Math.sqrt(81));   //9
+
+console.log(Math.abs(-2));  // 2
+
+console.log(Math.log(1));   
+console.log(Math.log10(200));
